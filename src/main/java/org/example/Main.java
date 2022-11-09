539315191dde7ab84b7e596ef33f6dc642d96527
S203932 <s203932@student.dtu.dk>
2022-11-09 16:45:39 +0100

    Tilføjet GUI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
-
+import gui_main.GUI;
 public class Main {
     public static void main(String[] args) {
+        GUI gui = new GUI();
         System.out.println("Hello world!");
     }
 }
