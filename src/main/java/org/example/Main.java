dc72b12cc64a97911f21e97614e228a995e21d77
Fayyad2016 <114231052+Fayyad2016@users.noreply.github.com>
2022-11-14 14:14:14 +0100

    Konto.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ import gui_main.GUI;
 public class Main {
     public static void main(String[] args) {
         GUI gui = new GUI();
-        System.out.println("Hello world!");
+        System.out.println("helle World");
     }
 }
