aa433d62140d3998302e0f1f5f5d7b90fe15229e
S203932 <s203932@student.dtu.dk>
2022-11-14 15:42:27 +0100

    Tilføjet read Txt metode Tilføjet Sprog  filer

--- src/main/java/CreateAndWriteToAndFromTXT/TestMain.java
+++ src/main/java/CreateAndWriteToAndFromTXT/TestMain.java
@@ -1,13 +1,23 @@
 package CreateAndWriteToAndFromTXT;
+
+import java.util.ArrayList;
+
 public class TestMain {
     public static void main(String[] args){
         // Testing the creating a file part
         CreateAFile createAFile = new CreateAFile();
-        createAFile.CreateAFile("Test");
+        createAFile.CreateAFile("Arabisk");
+
+        // Testing reading a file
+        ReadFile readFile = new ReadFile();
+        ArrayList<String> stringArrayList = readFile.ReadFile("TestSprog.txt");
+
+        System.out.println("Printing now the fetched arraylist from txt\n");
+        System.out.println(stringArrayList);
 
         // Testing writing to a file
-        WriteToFile writeToFile = new WriteToFile();
-        writeToFile.WriteToFile("Test.txt","I think it worked",3);
+        //WriteToFile writeToFile = new WriteToFile();
+        //writeToFile.WriteToFile("Test.txt","I think it worked",3);
 
 
 
