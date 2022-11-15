742785518ba0b3e9f84e3d1149153188deff5aff
S203932 <s203932@student.dtu.dk>
2022-11-15 11:34:30 +0100

    Added the constructer for ChanceCards

--- src/main/java/ChanceCardsAndDeck/ChanceCards.java
+++ src/main/java/ChanceCardsAndDeck/ChanceCards.java
@@ -8,12 +8,80 @@ public class ChanceCards {
     private int transaction;
 
     public ChanceCards(){
-
+        initilalizeType();
+        initilalizeDescription();
+        initilalizeMoveTo();
+        initilalizeMoveUpTo();
+        initilalizeTransaction();
     }
 
     public void initilalizeType(){
         this.type = -1;
     }
+
+    public void initilalizeDescription(){
+        this.description = "An empty description";
+    }
+
+    public void initilalizeMoveTo(){
+        this.moveTo = -1;
+    }
+
+    public void initilalizeMoveUpTo(){
+        this.moveUpTo = -1;
+    }
+
+    public void initilalizeTransaction(){
+        this.transaction = 0;
+    }
+
+    public void setType(int type){
+        this.type = type;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setMoveTo(int moveTo){
+        this.moveTo = moveTo;
+    }
+
+    public void setMoveUpTo(int moveUpTo){
+        this.moveUpTo = moveUpTo;
+    }
+
+    public void setTransaction(int transaction){
+        this.transaction = transaction;
+    }
+
+    public int getType(){
+        return this.type;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public int getMoveTo(){
+        return this.moveTo;
+    }
+
+    public int getMoveUpTo(){
+        return this.moveUpTo;
+    }
+
+    public int getTransaction(){
+        return this.transaction;
+    }
+
+    public String toString(){
+        return "This card has the following type: "+this.type+"\nThe following description: "+this.description+
+                "\nThe following MoveTo: "+this.moveTo+"\nThe following MoveUpTo: "+this.moveUpTo+"\n And the " +
+                "the following transaction: "+this.transaction;
+    }
+
+
 
 
 }
