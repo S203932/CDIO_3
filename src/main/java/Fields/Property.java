28c9aa28b56897dae41998f66afe769bc7918c92
zainabjh01 <91522375+zainabjh01@users.noreply.github.com>
2022-11-16 14:33:53 +0100

    Created Get and Set for properties

--- src/main/java/Fields/Property.java
+++ src/main/java/Fields/Property.java
@@ -2,5 +2,39 @@ package Fields;
 
 public class Property extends Field {
 
+// Constructor der opstiller en property       
+
+    private int value;
+    private boolean availability;
+    private int type;
+
+    public Property(){
+        setValue(0);
+        setAvailability(false);
+        setType(-1);
+
+    }
+    public void setValue(int value){
+        this.value = value;
+    }
+    public void setAvailability(boolean availability){
+        this.availability = availability;
+    }
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getValue() {
+          return this.value;
+    }
+
+    public boolean getAvailability() {
+        return this.availability;
+    }
+    public int getType(){
+        return this.type;
+    }
+
+    
 
 }
