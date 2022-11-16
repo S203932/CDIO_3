d8d58bd2cfada4f302d38339135055a74cb55336
misaki555 <113907659+misaki555@users.noreply.github.com>
2022-11-16 13:59:06 +0100

    Add CardDeck Class

--- src/main/java/ChanceCardsAndDeck/CardDeck.java
+++ src/main/java/ChanceCardsAndDeck/CardDeck.java
@@ -1,4 +1,32 @@
 package ChanceCardsAndDeck;
 
+import java.util.Arrays;
+
 public class CardDeck {
+    private ChanceCards[] card;
+
+    public CardDeck() {
+        initilalizeCard();
+    }
+
+    public void initilalizeCard(){
+        this.card=new ChanceCards[20];
+    }
+
+    public void setCard(ChanceCards[] card) {
+        this.card = card;
+    }
+
+    public ChanceCards getCard(int index) {
+        return card[index];
+    }
+
+    @Override
+    public String toString() {
+        String string= "";
+        for (int i=0; i<card.length; i++){
+            string=string+card[i].toString()+"\n";
+        }
+        return "Chence cards" + string;
+    }
 }
