6dbde84fcc35150eb264f766902db9d41b5fdd7f
zainabjh01 <91522375+zainabjh01@users.noreply.github.com>
2022-11-18 13:54:36 +0100

    Changes in FieldList

--- src/main/java/Fields/FieldList.java
+++ src/main/java/Fields/FieldList.java
@@ -25,31 +25,31 @@ public class FieldList {
         ((StartField)fieldList[0]).setColor(Color.LIGHT_GRAY);
 
         fieldList[1] = new Property();
-        ((Property)fieldList[1]).setName("BURGERBAREN");
+        ((Property)fieldList[1]).setName(stringArrayList.get(1));
         ((Property)fieldList[1]).setValue(1);
         ((Property)fieldList[1]).setColor(Color.ORANGE);
 
         fieldList[2] = new Property();
-        ((Property)fieldList[2]).setName("PIZZAHUSET");
+        ((Property)fieldList[2]).setName(stringArrayList.get(2));
         ((Property)fieldList[2]).setValue(1);
         ((Property)fieldList[2]).setColor(Color.ORANGE);
 
         fieldList[3] = new Chance();
-        ((Chance)fieldList[3]).setName("CHANCE");
+        ((Chance)fieldList[3]).setName(stringArrayList.get(3));
         ((Chance)fieldList[3]).setColor(Color.LIGHT_GRAY);
 
         fieldList[4] = new Property();
-        ((Property)fieldList[4]).setName("SLIKBUTIKKEN");
+        ((Property)fieldList[4]).setName(stringArrayList.get(4));
         ((Property)fieldList[4]).setValue(1);
         ((Property)fieldList[4]).setColor(Color.GRAY);
 
         fieldList[5] = new Property();
-        ((Property)fieldList[5]).setName("ISKIOSKEN");
+        ((Property)fieldList[5]).setName(stringArrayList.get(5));
         ((Property)fieldList[5]).setValue(1);
         ((Property)fieldList[5]).setColor(Color.GRAY);
 
         fieldList[6] = new NeutralField();
-        ((NeutralField)fieldList[6]).setName("BESØG I FAENGSEL");
+        ((NeutralField)fieldList[6]).setName(stringArrayList.get(6));
         ((NeutralField)fieldList[6]).setColor(Color.CYAN);
         ((Chance)fieldList[3]).setName("Chance");
     }
