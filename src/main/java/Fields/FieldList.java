b10824366db1e54eff8b5c6970832c257fc8df1a
KrisUrb <s163301@student.dtu.dk>
2022-11-18 14:17:55 +0100

    Adding 24 Fields, setup for connecting to txt language packs for all

--- src/main/java/Fields/FieldList.java
+++ src/main/java/Fields/FieldList.java
@@ -21,37 +21,116 @@ public class FieldList {
         ReadFile readFile = new ReadFile();
         ArrayList<String> stringArrayList = readFile.ReadFile(filename);
         fieldList[0] = new StartField();
-        ((StartField)fieldList[0]).setName(stringArrayList.get(0));
+        ((StartField)fieldList[0]).setName(stringArrayList.get(1));
         ((StartField)fieldList[0]).setColor(Color.LIGHT_GRAY);
 
         fieldList[1] = new Property();
-        ((Property)fieldList[1]).setName(stringArrayList.get(1));
+        ((Property)fieldList[1]).setName(stringArrayList.get(2));
         ((Property)fieldList[1]).setValue(1);
         ((Property)fieldList[1]).setColor(Color.ORANGE);
 
         fieldList[2] = new Property();
-        ((Property)fieldList[2]).setName(stringArrayList.get(2));
+        ((Property)fieldList[2]).setName(stringArrayList.get(3));
         ((Property)fieldList[2]).setValue(1);
         ((Property)fieldList[2]).setColor(Color.ORANGE);
 
         fieldList[3] = new Chance();
-        ((Chance)fieldList[3]).setName(stringArrayList.get(3));
+        ((Chance)fieldList[3]).setName(stringArrayList.get(4));
         ((Chance)fieldList[3]).setColor(Color.LIGHT_GRAY);
 
         fieldList[4] = new Property();
-        ((Property)fieldList[4]).setName(stringArrayList.get(4));
+        ((Property)fieldList[4]).setName(stringArrayList.get(5));
         ((Property)fieldList[4]).setValue(1);
         ((Property)fieldList[4]).setColor(Color.GRAY);
 
         fieldList[5] = new Property();
-        ((Property)fieldList[5]).setName(stringArrayList.get(5));
+        ((Property)fieldList[5]).setName(stringArrayList.get(6));
         ((Property)fieldList[5]).setValue(1);
         ((Property)fieldList[5]).setColor(Color.GRAY);
 
         fieldList[6] = new NeutralField();
-        ((NeutralField)fieldList[6]).setName(stringArrayList.get(6));
-        ((NeutralField)fieldList[6]).setColor(Color.CYAN);
-        ((Chance)fieldList[3]).setName("Chance");
+        ((NeutralField)fieldList[6]).setName(stringArrayList.get(7));
+        ((NeutralField)fieldList[6]).setColor(Color.LIGHT_GRAY);
+
+        fieldList[7] = new Property();
+        ((Property)fieldList[7]).setName(stringArrayList.get(8));
+        ((Property)fieldList[7]).setValue(2);
+        ((Property)fieldList[7]).setColor(Color.MAGENTA);
+
+        fieldList[8] = new Property();
+        ((Property)fieldList[8]).setName(stringArrayList.get(9));
+        ((Property)fieldList[8]).setValue(2);
+        ((Property)fieldList[8]).setColor(Color.MAGENTA);
+
+        fieldList[9] = new Chance();
+        ((NeutralField)fieldList[9]).setName(stringArrayList.get(10));
+        ((NeutralField)fieldList[9]).setColor(Color.LIGHT_GRAY);
+
+        fieldList[10] = new Property();
+        ((Property)fieldList[10]).setName(stringArrayList.get(11));
+        ((Property)fieldList[10]).setValue(2);
+        ((Property)fieldList[10]).setColor(Color.YELLOW);
+
+        fieldList[11] = new Property();
+        ((Property)fieldList[11]).setName(stringArrayList.get(12));
+        ((Property)fieldList[11]).setValue(2);
+        ((Property)fieldList[11]).setColor(Color.YELLOW);
+
+        fieldList[12] = new NeutralField();
+        ((NeutralField)fieldList[12]).setName(stringArrayList.get(11));
+        ((NeutralField)fieldList[12]).setColor(Color.LIGHT_GRAY);
+
+        fieldList[13] = new Property();
+        ((Property)fieldList[13]).setName(stringArrayList.get(14));
+        ((Property)fieldList[13]).setValue(3);
+        ((Property)fieldList[13]).setColor(Color.PINK);
+
+        fieldList[14] = new Property();
+        ((Property)fieldList[14]).setName(stringArrayList.get(15));
+        ((Property)fieldList[14]).setValue(3);
+        ((Property)fieldList[14]).setColor(Color.PINK);
+
+        fieldList[15] = new Chance();
+        ((NeutralField)fieldList[15]).setName(stringArrayList.get(16));
+        ((NeutralField)fieldList[15]).setColor(Color.LIGHT_GRAY);
+
+        fieldList[16] = new Property();
+        ((Property)fieldList[16]).setName(stringArrayList.get(17));
+        ((Property)fieldList[16]).setValue(3);
+        ((Property)fieldList[16]).setColor(Color.ORANGE);
+
+        fieldList[17] = new Property();
+        ((Property)fieldList[17]).setName(stringArrayList.get(18));
+        ((Property)fieldList[17]).setValue(3);
+        ((Property)fieldList[17]).setColor(Color.ORANGE);
+
+        fieldList[18] = new GoJail();
+        ((NeutralField)fieldList[18]).setName(stringArrayList.get(19));
+        ((NeutralField)fieldList[18]).setColor(Color.LIGHT_GRAY);
+
+        fieldList[19] = new Property();
+        ((Property)fieldList[19]).setName(stringArrayList.get(20));
+        ((Property)fieldList[19]).setValue(4);
+        ((Property)fieldList[19]).setColor(Color.GREEN);
+
+        fieldList[20] = new Property();
+        ((Property)fieldList[20]).setName(stringArrayList.get(21));
+        ((Property)fieldList[20]).setValue(4);
+        ((Property)fieldList[20]).setColor(Color.GREEN);
+
+        fieldList[21] = new Chance();
+        ((NeutralField)fieldList[21]).setName(stringArrayList.get(22));
+        ((NeutralField)fieldList[21]).setColor(Color.LIGHT_GRAY);
+
+        fieldList[22] = new Property();
+        ((Property)fieldList[22]).setName(stringArrayList.get(23));
+        ((Property)fieldList[22]).setValue(5);
+        ((Property)fieldList[22]).setColor(Color.BLUE);
+
+        fieldList[23] = new Property();
+        ((Property)fieldList[23]).setName(stringArrayList.get(25));
+        ((Property)fieldList[23]).setValue(5);
+        ((Property)fieldList[23]).setColor(Color.BLUE);
     }
     public Field getFieldIndex(int index){
         return fieldList[index];
